9fc8b809f6965480d916f5bb85f0bd1addb7324b
Michael Primavera <pastasauce59@gmail.com>
2023-02-21 23:59:44 -0500

    created a numbers list and a range loop to populate list with numbers, created number variable to select random number from numbers list, created difficulty input to capture how many attemps the user has to guess, created a while loop with if statements to determine if user guessed correctly and tell how many attemps are left.

--- number_guess.py
+++ number_guess.py
@@ -1,3 +1,29 @@
 from art import logo
+import random
 print(logo)
 print("Welcome to the Number Guessing Game!")
+
+numbers = []
+for n in range(1, 101):
+    numbers.append(n)
+
+print("I'm thinking of a number between 1 and 100.")
+number =  random.choice(numbers)
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+if difficulty == 'easy':
+    attemps = 10
+else:
+    attemps = 5
+
+while attemps != 0:
+    guess = int(input("Make a guess: "))
+    if guess > number:
+        print("Too high.")
+        attemps -= 1
+        print(f"You have {attemps} attemps remaining to guess the number")
+    elif guess < number:
+        print("Too low")
+        attemps -= 1
+        print(f"You have {attemps} attemps remaining to guess the number")
+    elif guess == number:
+        print("You got it!.")
