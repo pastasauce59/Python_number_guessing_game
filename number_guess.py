94eef0fcf32c7520a5f53fe78945872038d584cd
Michael Primavera <pastasauce59@gmail.com>
2023-02-22 12:57:20 -0500

    greatly simpilfied the if-elif statements in while loop to remove repeated code, added additional print statements that provide the user more feedback.

--- number_guess.py
+++ number_guess.py
@@ -9,6 +9,10 @@ for n in range(1, 101):
 
 print("I'm thinking of a number between 1 and 100.")
 number =  random.choice(numbers)
+
+#FOR TESTING
+print(f"psst, the number is {number}")
+
 difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
 if difficulty == 'easy':
     attemps = 10
@@ -16,14 +20,37 @@ else:
     attemps = 5
 
 while attemps != 0:
+    print(f"You have {attemps} attemps remaining to guess the number.")
     guess = int(input("Make a guess: "))
-    if guess > number:
-        print("Too high.")
+    # if guess > number:
+    #     print("Too high.")
+    #     attemps -= 1
+    #     if attemps == 0:
+    #         print("You've run out of guesses, you lose.")
+    #     else:
+    #         print("Guess again.")
+    # elif guess < number:
+    #     print("Too low.")
+    #     attemps -= 1
+    #     if attemps == 0:
+    #         print("You've run out of guesses, you lose.")
+    #     else:
+    #         print("Guess again.")
+    # elif guess == number:
+    #     print(f"You got it!. The answer was {number}.")
+    #     attemps = 0
+
+    if guess != number:
         attemps -= 1
-        print(f"You have {attemps} attemps remaining to guess the number")
-    elif guess < number:
-        print("Too low")
-        attemps -= 1
-        print(f"You have {attemps} attemps remaining to guess the number")
-    elif guess == number:
-        print("You got it!.")
+        if guess > number:
+            print("Too high.")
+        elif guess < number:
+            print("Too low.")
+        elif guess == number:
+            print(f"You got it!. The answer was {number}.")
+            attemps = 0
+        
+        if attemps != 0:
+            print("Guess again.")
+        else:
+            print("You've run out of guesses, you lose.")
